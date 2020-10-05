21ceb1bac2b055ad36136ac934b6a838511b1d18
mmtakeuchi <mikemtaki@gmail.com>
2020-10-05 15:36:18 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+// ./src/index.js
+ 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import manageTodo from './reducers/manageTodo';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
+ 
 let store = createStore(manageTodo);
-
+ 
+ 
 ReactDOM.render(
   <Provider store={store}>
     <App />
